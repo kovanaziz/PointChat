75b86d3a031b118e0086cde5a5ee8c0620103389
kovan <kovan.aziz08@gmail.com>
2018-01-22 05:18:39 -0800

    data service initialized with a func for creating users

---
+++ point/Services/DataService.swift
@@ -0,0 +1,45 @@
+//
+//  DataService.swift
+//  point
+//
+//  Created by John Smith on 1/22/18.
+//  Copyright © 2018 John Smith. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// base url for database
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE : DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERs : DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS : DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED : DatabaseReference {
+        return _REF_FEED
+    }
+    
+    // this func can be used to create firebase users
+    func createDBUSER(uid:String, userData: Dictionary<String,Any>) {
+        REF_USERs.child(uid).updateChildValues(userData)
+        
+    }
+}
