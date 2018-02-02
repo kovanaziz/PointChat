6d7ca36110c5781c99793d84a13d3536568a3449
kovan <kovan.aziz08@gmail.com>
2018-02-02 09:12:03 -0800

    user profileVC created

---
+++ point/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  point
+//
+//  Created by John Smith on 2/2/18.
+//  Copyright © 2018 John Smith. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        //sigoutcode
+    }
+    
+
+}
